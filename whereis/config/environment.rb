c3c8d3b3dec9200dad4150363a4aaff050ecc7c7
Eunchul Jung <agfe09@gmail.com>
2010-04-09 21:01:07 +0900

    add gems: rsolr

--- whereis/config/environment.rb
+++ whereis/config/environment.rb
@@ -21,6 +21,7 @@ Rails::Initializer.run do |config|
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem "outoftime-sunspot", :lib => "sunspot"
   config.gem "outoftime-sunspot_rails", :lib => "sunspot/rails"
+  config.gem "mwmitchell-rsolr", :lib => "rsolr"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
